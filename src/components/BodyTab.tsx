af03f8fa0607013b59dcbc923d3a0be63d38c507
astabile <alex.stabile@lastminute.com>
2022-02-01 14:07:01 +0100

    Some styling

--- src/components/BodyTab.tsx
+++ src/components/BodyTab.tsx
@@ -2,9 +2,15 @@ import React, { FC } from 'react';
 import styled from 'styled-components';
 
 const TextArea = styled.textarea`
+	background-color: ${p => p.theme.dark4};
+	color: ${p => p.theme.light1};
 	width: 100%;
 	height: 500px;
 	padding: 8px;
+
+	&:focus {
+		outline: none;
+	}
 `;
 
 export const BodyTab: FC = () => <TextArea>Some random content</TextArea>;
