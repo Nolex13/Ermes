ca0b469dd6ee9bb6124aaf4c64d9992fd2914bb3
astabile <alex.stabile@lastminute.com>
2022-02-07 14:06:26 +0100

    Minors

--- deploy.ts
+++ deploy.ts
@@ -1,18 +1,18 @@
-const ghpages = require("gh-pages");
+const ghpages = require('gh-pages');
 
-console.log("Publishing on Github pages");
+console.log('Publishing on Github pages');
 
 ghpages.publish(
-  "./build",
-  {
-    branch: "main",
-    repo: "git@github.com:Nolex13/Nolex13.github.io.git"
-  },
-  (err) => {
-		if(err !== undefined){
-			console.error("Error publishing the project to Github pages", err);
+	'./build',
+	{
+		branch: 'main',
+		repo: 'git@github.com:Nolex13/Nolex13.github.io.git',
+	},
+	err => {
+		if (err !== undefined) {
+			console.error('Error publishing the project to Github pages', err);
 		} else {
-			console.log("Access to: https://nolex13.github.io/");
+			console.log('Access to: https://nolex13.github.io/');
 		}
-  }
+	},
 );
