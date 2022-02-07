134cc10a7d9814a3b0fb65c2e1da576848a1e8a4
astabile <alex.stabile@lastminute.com>
2022-02-07 08:48:31 +0100

    Adding some style to the blank page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,13 +4,13 @@ import { useAppSelector } from '../data/Hooks';
 import { getActivePage } from '../data/slices/PagesSlice';
 import { Paginator } from './Paginator/Paginator';
 import { RequestBody } from './RequestBody';
+import img from '../images/increase-productivity.png';
 
 const Container = styled.div`
 	background: ${props => props.theme.dark4};
 	color: ${props => props.theme.light1};
 	flex: 1 1;
 	padding: 16px;
-	min-height: 100%;
 `;
 
 const Wrapper = styled.div`
@@ -18,6 +18,72 @@ const Wrapper = styled.div`
 	flex: 1;
 	flex-direction: column;
 `;
+
+const Img = styled.div`
+	background-image: url(${img});
+	background-size: contain;
+	background-repeat: no-repeat;
+	background-position: center center;
+	height: 45%;
+	width: 45%;
+`;
+
+const OpenRequestContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	align-items: center;
+	justify-content: center;
+
+	& h3,
+	& h4 {
+		font-weight: 300;
+		font-size: 2.5em;
+	}
+
+	& h4 {
+		margin-top: 16px;
+		font-size: 2em;
+	}
+
+	& h3 {
+		overflow: hidden; /* Ensures the content is not revealed until the animation */
+		border-right: 0.15em solid orange; /* The typwriter cursor */
+		white-space: nowrap; /* Keeps the content on a single line */
+		margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+		letter-spacing: 0.15em; /* Adjust as needed */
+		animation: typing 3.5s steps(26, end), blink-caret 0.75s step-end infinite;
+	}
+
+	/* The typing effect */
+	@keyframes typing {
+		from {
+			width: 0;
+		}
+		to {
+			width: 600px;
+		}
+	}
+
+	/* The typewriter cursor effect */
+	@keyframes blink-caret {
+		from,
+		to {
+			border-color: transparent;
+		}
+		50% {
+			border-color: orange;
+		}
+	}
+`;
+
+const OpenRequest: FC = () => (
+	<OpenRequestContainer>
+		<h3>Increase your productivity</h3>
+		<h4>Create or open a request to start</h4>
+		<Img />
+	</OpenRequestContainer>
+);
 
 export const Main: FC = () => {
 	const pagination = useAppSelector(state => state.pagination);
@@ -29,6 +95,7 @@ export const Main: FC = () => {
 				{pagination.pages.length > 0 && (
 					<RequestBody request={getActivePage(pagination)} />
 				)}
+				{pagination.pages.length === 0 && <OpenRequest />}
 			</Container>
 		</Wrapper>
 	);
